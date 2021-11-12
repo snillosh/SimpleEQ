b092894d397bf7d5279e4ca10a313e838f32243d
snillosh <69086485+snillosh@users.noreply.github.com>
2021-11-12 09:47:45 +0000

    Commit Test

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -54,6 +54,9 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    
+    //change test
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
